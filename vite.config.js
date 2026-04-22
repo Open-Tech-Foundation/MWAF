dc6d3de519403e0b1c564fee00f8e8dfd1e8dd79
Thanga Ganapathy <ganapathy888@gmail.com>
2026-04-22 12:22:08 +0530

    feat: integrate Cloudflare Pages with wrangler and vite-plugin-cloudflare

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,20 @@ import { babel } from '@rollup/plugin-babel'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
   esbuild: {
     jsx: 'preserve'
   },
-  plugins: [
-    tailwindcss(),
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      exclude: 'node_modules/**',
-      configFile: false,
-      plugins: [
-        "@babel/plugin-syntax-jsx",
-        [path.resolve(__dirname, 'framework/compiler/babel-plugin.cjs')]
-      ]
-    })
-  ]
+  plugins: [tailwindcss(), babel({
+    babelHelpers: 'bundled',
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    exclude: 'node_modules/**',
+    configFile: false,
+    plugins: [
+      "@babel/plugin-syntax-jsx",
+      [path.resolve(__dirname, 'framework/compiler/babel-plugin.cjs')]
+    ]
+  }), cloudflare()]
 })
